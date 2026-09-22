fa5a7059868fde6a244cdc20abfd14ab51d0aceb
rogith-3018 <rogith3018@gmail.com>
2026-09-21 22:56:14 -0700

    Add files via upload

---
+++ rogith.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class rogith
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc = new Scanner(System.in);
+	   int a =sc.nextInt();
+	   int b = sc.nextInt();
+	   System.out.print((a*b)*100);
+
+	}
+}
